905f81949d43bd70e24c2fe9cbfdfddb66e47f92
zatagado <zachg@vt.edu>
2023-11-28 00:31:13 -0500

    Fixed issue with vector4 in blinn-phong. Started working on changes to pipeline for outline.

--- renderer.py
+++ renderer.py
@@ -109,7 +109,7 @@ class Renderer:
             depth_norm = (depth + 1) / 2
             return (255 * depth_norm, 255 * depth_norm, 255 * depth_norm)
         
-        def shade_phong_blinn(camera, light, ambient_light, shadow_map, mesh, world_tri, world_tri_vert_normals, alpha, beta, gamma):
+        def shade_phong_blinn(camera, light, ambient_light, shadow_map, mesh, ndc_tri, world_tri, world_tri_vert_normals, alpha, beta, gamma):
             o = mesh.diffuse_color
             kd = mesh.kd
             l_color = light.color
@@ -129,7 +129,7 @@ class Renderer:
                         Vector4.mul(Vector4.div(Vector3.to_Vector4(world_tri[2]), perspective_correction_w(self.camera, ndc_tri[2])), gamma)))
                     p_sm = Vector4.to_Vector3(Vector4.div(p_sm, p_sm[3]))
                 else:
-                    p_sm = Vector3.add(Vector3.mul(world_tri[0], alpha), Vector3.add(Vector4.mul(world_tri[1], beta), \
+                    p_sm = Vector3.add(Vector3.mul(world_tri[0], alpha), Vector3.add(Vector3.mul(world_tri[1], beta), \
                         Vector3.mul(world_tri[2], gamma)))
                 in_light = shadow_map.check_occlusion(p_sm)
 
@@ -236,7 +236,7 @@ class Renderer:
 
             return (texture_pixels[x, y][0], texture_pixels[x, y][1], texture_pixels[x, y][2])
 
-        def shade_shadow_map(camera, shadow_map, world_tri, alpha, beta, gamma):
+        def shade_shadow_map(camera, shadow_map, ndc_tri, world_tri, alpha, beta, gamma):
             p = None
             if isinstance(camera, PerspectiveCamera):
                 p = Vector4.add(Vector4.mul(Vector4.div(Vector3.to_Vector4(world_tri[0]), perspective_correction_w(self.camera, ndc_tri[0])), alpha), \
@@ -249,7 +249,7 @@ class Renderer:
             in_light = shadow_map.check_occlusion(p)
             return (255 * in_light, 255 * in_light, 255 * in_light)
 
-        def shade_stylized(camera, light, ambient_light, shadow_map, mesh, world_tri, world_tri_vert_normals, alpha, beta, gamma):
+        def shade_stylized(camera, light, ambient_light, shadow_map, mesh, ndc_tri, world_tri, world_tri_vert_normals, alpha, beta, gamma):
             o = mesh.diffuse_color
             kd = mesh.kd
             l_color = light.color
@@ -340,115 +340,125 @@ class Renderer:
         image_buffer = np.full((self.screen.width, self.screen.height, 3), bg_color)
         depth_buffer = np.full((self.screen.width, self.screen.height), -math.inf, dtype=float)
 
-        for i in range(len(self.meshes)):
-            mesh: Mesh = self.meshes[i]
+        def render_pass(image_buffer, depth_buffer, meshes, shading):
+            for i in range(len(meshes)):
+                mesh: Mesh = meshes[i]
 
-            world_verts = [mesh.transform.apply_to_point(p) for p in mesh.verts]
-            ndc_verts = [self.camera.project_point(p) for p in world_verts]
-            screen_verts = [self.screen.device_to_screen(p) for p in ndc_verts]
-            
-            vertex_colors = [None] * len(mesh.verts) if shading == "gouraud" else None
+                world_verts = [mesh.transform.apply_to_point(p) for p in mesh.verts]
+                ndc_verts = [self.camera.project_point(p) for p in world_verts]
+                screen_verts = [self.screen.device_to_screen(p) for p in ndc_verts]
+                
+                vertex_colors = [None] * len(mesh.verts) if shading == "gouraud" else None
 
-            texture_pixels = None
-            texture_width = None
-            texture_height = None
-            if shading == "texture" or shading == "texture-correct":
-                texture_pixels = mesh.texture.load()
-                texture_width = mesh.texture.width
-                texture_height = mesh.texture.height
+                texture_pixels = None
+                texture_width = None
+                texture_height = None
+                if shading == "texture" or shading == "texture-correct":
+                    texture_pixels = mesh.texture.load()
+                    texture_width = mesh.texture.width
+                    texture_height = mesh.texture.height
 
-            for j in range(len(mesh.faces)):
-                face = mesh.faces[j]
-                normal = mesh.normals[j]
-                world_tri_vert_normals = [mesh.vert_normals[face[0]], mesh.vert_normals[face[1]], mesh.vert_normals[face[2]]] if len(mesh.vert_normals) > 0 else None
-                world_tri = [world_verts[face[0]], world_verts[face[1]], world_verts[face[2]]]
-                ndc_tri = [ndc_verts[face[0]], ndc_verts[face[1]], ndc_verts[face[2]]]
-                screen_tri = [screen_verts[face[0]], screen_verts[face[1]], screen_verts[face[2]]]
-                uv_tri = [mesh.uvs[face[0]], mesh.uvs[face[1]], mesh.uvs[face[2]]] if len(mesh.uvs) > 0 else None
+                for j in range(len(mesh.faces)):
+                    face = mesh.faces[j]
+                    normal = mesh.normals[j]
+                    world_tri_vert_normals = [mesh.vert_normals[face[0]], mesh.vert_normals[face[1]], mesh.vert_normals[face[2]]] if len(mesh.vert_normals) > 0 else None
+                    world_tri = [world_verts[face[0]], world_verts[face[1]], world_verts[face[2]]]
+                    ndc_tri = [ndc_verts[face[0]], ndc_verts[face[1]], ndc_verts[face[2]]]
+                    screen_tri = [screen_verts[face[0]], screen_verts[face[1]], screen_verts[face[2]]]
+                    uv_tri = [mesh.uvs[face[0]], mesh.uvs[face[1]], mesh.uvs[face[2]]] if len(mesh.uvs) > 0 else None
 
-                # cull triangle if normal is facing away from camera
-                cross = Vector3.cross(Vector3.normalize(Vector3.sub(ndc_tri[1], ndc_tri[0])), \
-                    Vector3.normalize(Vector3.sub(ndc_tri[2], ndc_tri[0])))
-                if cross[2] < 0:
-                    continue
+                    # cull triangle if normal is facing away from camera
+                    cross = Vector3.cross(Vector3.normalize(Vector3.sub(ndc_tri[1], ndc_tri[0])), \
+                        Vector3.normalize(Vector3.sub(ndc_tri[2], ndc_tri[0])))
+                    if cross[2] < 0:
+                        continue
 
-                # get pixel bounds for which areas of pixels should be drawn
-                bounds = get_pixel_bounds(screen_tri, self.screen.width, self.screen.height)
-                x_min = bounds[0][0]
-                x_max = bounds[1][0]
-                y_min = bounds[0][1]
-                y_max = bounds[1][1]
+                    # get pixel bounds for which areas of pixels should be drawn
+                    bounds = get_pixel_bounds(screen_tri, self.screen.width, self.screen.height)
+                    x_min = bounds[0][0]
+                    x_max = bounds[1][0]
+                    y_min = bounds[0][1]
+                    y_max = bounds[1][1]
 
-                x_a = screen_tri[0][0]
-                x_b = screen_tri[1][0]
-                x_c = screen_tri[2][0]
-                y_a = screen_tri[0][1]
-                y_b = screen_tri[1][1]
-                y_c = screen_tri[2][1]
+                    x_a = screen_tri[0][0]
+                    x_b = screen_tri[1][0]
+                    x_c = screen_tri[2][0]
+                    y_a = screen_tri[0][1]
+                    y_b = screen_tri[1][1]
+                    y_c = screen_tri[2][1]
 
-                vert_color_tri = None
-                if shading == "gouraud":
-                    shade_gouraud_vertex(vertex_colors, face, ambient_light, self.light, self.camera, mesh, \
-                        world_tri, world_tri_vert_normals) # face index
-                    vert_color_tri = [vertex_colors[face[0]], vertex_colors[face[1]], vertex_colors[face[2]]]
+                    vert_color_tri = None
+                    if shading == "gouraud":
+                        shade_gouraud_vertex(vertex_colors, face, ambient_light, self.light, self.camera, mesh, \
+                            world_tri, world_tri_vert_normals) # face index
+                        vert_color_tri = [vertex_colors[face[0]], vertex_colors[face[1]], vertex_colors[face[2]]]
 
-                for x in range(x_min, x_max):
-                    for y in range(y_min, y_max):
-                        # barycentric coordinate checking
-                        # ? What happens when there is a negative divisor
-                        gammaDivisor = (((y_a - y_b) * x_c) + ((x_b - x_a) * y_c) + (x_a * y_b) - (x_b * y_a))
-                        if gammaDivisor == 0:
-                            gammaDivisor = 0.0000001
-                        gamma = (((y_a - y_b) * x) + ((x_b - x_a) * y) + (x_a * y_b) - (x_b * y_a)) / gammaDivisor
+                    for x in range(x_min, x_max):
+                        for y in range(y_min, y_max):
+                            # barycentric coordinate checking
+                            # ? What happens when there is a negative divisor
+                            gammaDivisor = (((y_a - y_b) * x_c) + ((x_b - x_a) * y_c) + (x_a * y_b) - (x_b * y_a))
+                            if gammaDivisor == 0:
+                                gammaDivisor = 0.0000001
+                            gamma = (((y_a - y_b) * x) + ((x_b - x_a) * y) + (x_a * y_b) - (x_b * y_a)) / gammaDivisor
+                                
+                            betaDivisor = (((y_a - y_c) * x_b) + ((x_c - x_a) * y_b) + (x_a * y_c) - (x_c * y_a))
+                            if betaDivisor == 0:
+                                betaDivisor = 0.0000001
+                            beta = (((y_a - y_c) * x) + ((x_c - x_a) * y) + (x_a * y_c) - (x_c * y_a)) / betaDivisor
+
+                            alpha = 1 - beta - gamma
+
+                            # ignore pixel outside of triangle
+                            if gamma < 0 or beta < 0 or alpha < 0:
+                                continue
+
+                            # ignore pixel outside near far bounds
+                            depth = Vector3.add(Vector3.mul(ndc_tri[0], alpha), Vector3.add(Vector3.mul(ndc_tri[1], beta), Vector3.mul(ndc_tri[2], gamma)))[2]
+                            if depth > 1 or depth < -1: #? should the first be 1 instead of 0????
+                                continue
+
+                            # render the pixel depending on the depth of the pixel
+                            if (depth + 1) / 2 <= depth_buffer[x, y]:
+                                continue
                             
-                        betaDivisor = (((y_a - y_c) * x_b) + ((x_c - x_a) * y_b) + (x_a * y_c) - (x_c * y_a))
-                        if betaDivisor == 0:
-                            betaDivisor = 0.0000001
-                        beta = (((y_a - y_c) * x) + ((x_c - x_a) * y) + (x_a * y_c) - (x_c * y_a)) / betaDivisor
+                            # add depth of pixel
+                            depth_buffer[x, y] = (depth + 1) / 2
 
-                        alpha = 1 - beta - gamma
+                            # render the pixel
+                            if shading == "flat":
+                                image_buffer[x, y] = shade_flat(self.light, ambient_light, mesh, \
+                                    world_tri, normal, alpha, beta, gamma)
+                            elif shading == "barycentric":
+                                image_buffer[x, y] = shade_barycentric(alpha, beta, gamma)
+                            elif shading == "depth":
+                                image_buffer[x, y] = shade_depth(depth)
+                            elif shading == "phong-blinn":
+                                image_buffer[x, y] = shade_phong_blinn(self.camera, self.light, ambient_light, self.shadow_map, \
+                                    mesh, ndc_tri, world_tri, world_tri_vert_normals, alpha, beta, gamma)
+                            elif shading == "gouraud":
+                                image_buffer[x, y] = shade_gouraud_pixel(vert_color_tri, alpha, beta, gamma)
+                            elif shading == "texture":
+                                image_buffer[x, y] = shade_texture(texture_pixels, texture_width, texture_height, uv_tri, \
+                                    alpha, beta, gamma)
+                            elif shading == "texture-correct":
+                                image_buffer[x, y] = shade_texture_correct(self.camera, texture_pixels, texture_width, texture_height, uv_tri, \
+                                    ndc_tri, alpha, beta, gamma)
+                            elif shading == "shadow-map":
+                                image_buffer[x, y] = shade_shadow_map(self.camera, self.shadow_map, ndc_tri, world_tri, alpha, beta, gamma)
+                            elif shading == "stylized":
+                                image_buffer[x, y] = shade_stylized(self.camera, self.light, ambient_light, self.shadow_map, \
+                                    mesh, ndc_tri, world_tri, world_tri_vert_normals, alpha, beta, gamma)
+                            elif shading == "outline":
+                                image_buffer
 
-                        # ignore pixel outside of triangle
-                        if gamma < 0 or beta < 0 or alpha < 0:
-                            continue
+                if shading == "texture" or shading == "texture-correct":
+                    mesh.texture.close()
 
-                        # ignore pixel outside near far bounds
-                        depth = Vector3.add(Vector3.mul(ndc_tri[0], alpha), Vector3.add(Vector3.mul(ndc_tri[1], beta), Vector3.mul(ndc_tri[2], gamma)))[2]
-                        if depth > 1 or depth < -1: #? should the first be 1 instead of 0????
-                            continue
+        def invert_hull(meshes):
+            inverted_hull_meshes = []
+            return inverted_hull_meshes
 
-                        # render the pixel depending on the depth of the pixel
-                        if (depth + 1) / 2 <= depth_buffer[x, y]:
-                            continue
-                        
-                        # add depth of pixel
-                        depth_buffer[x, y] = (depth + 1) / 2
-
-                        # render the pixel
-                        if shading == "flat":
-                            image_buffer[x, y] = shade_flat(self.light, ambient_light, mesh, \
-                                world_tri, normal, alpha, beta, gamma)
-                        elif shading == "barycentric":
-                            image_buffer[x, y] = shade_barycentric(alpha, beta, gamma)
-                        elif shading == "depth":
-                            image_buffer[x, y] = shade_depth(depth)
-                        elif shading == "phong-blinn":
-                            image_buffer[x, y] = shade_phong_blinn(self.camera, self.light, ambient_light, self.shadow_map, \
-                                mesh, world_tri, world_tri_vert_normals, alpha, beta, gamma)
-                        elif shading == "gouraud":
-                            image_buffer[x, y] = shade_gouraud_pixel(vert_color_tri, alpha, beta, gamma)
-                        elif shading == "texture":
-                            image_buffer[x, y] = shade_texture(texture_pixels, texture_width, texture_height, uv_tri, \
-                                alpha, beta, gamma)
-                        elif shading == "texture-correct":
-                            image_buffer[x, y] = shade_texture_correct(self.camera, texture_pixels, texture_width, texture_height, uv_tri, \
-                                ndc_tri, alpha, beta, gamma)
-                        elif shading == "shadow-map":
-                            image_buffer[x, y] = shade_shadow_map(self.camera, self.shadow_map, world_tri, alpha, beta, gamma)
-                        elif shading == "stylized":
-                            image_buffer[x, y] = shade_stylized(self.camera, self.light, ambient_light, self.shadow_map, \
-                                mesh, world_tri, world_tri_vert_normals, alpha, beta, gamma)
-
-            if shading == "texture" or shading == "texture-correct":
-                mesh.texture.close()
+        render_pass(image_buffer, depth_buffer, invert_hull(self.meshes), "outline")
+        render_pass(image_buffer, depth_buffer, self.meshes, shading)
         self.screen.draw(image_buffer)
